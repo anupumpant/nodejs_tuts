28b6d1f57b517a6ea09f222c9a0946e81aa7c48f
anupumpant <indigoanalysis@gmail.com>
2024-05-04 15:05:38 -0700

    modified:   .gitignore 	modified:   express-server.js 	new file:   models/blog.js 	modified:   package-lock.json 	modified:   package.json

--- express-server.js
+++ express-server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Blog = require("./models/blog.js");
 
 const app = express();
-
 app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.listen(3000, () => {
-  console.log("app is listening");
-});
+mongoose
+  .connect(process.env.dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("Connected to db");
+
+    app.listen(process.env.PORT, () => {
+      console.log("app is listening");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   //res.sendFile("./views/index.html", { root: __dirname });
@@ -27,6 +42,33 @@ app.get("/about", (req, res) => {
 
 app.get("/index", (req, res) => {
   res.redirect("/");
+});
+
+app.get("/create-blog", (req, res) => {
+  const blog = new Blog({
+    title: "This is the blog headline",
+    snippet: "This is the snippet",
+    body: "The body of the blog is shown here",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("6636976d229705dbbcfbfd4a")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.use((req, res) => {
